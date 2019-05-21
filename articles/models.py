7ddac0b09e6994a037cccb1c61a22057b587b139
Denis Shipkov <ya@gremli.ru>
2019-05-21 13:33:38 +0300

    #1 new db requests #2 mutable article formats

--- articles/models.py
+++ articles/models.py
@@ -1,15 +1,18 @@
 from django.db import models
 
 class Article(models.Model):
+    FORMAT_CHOICES = [('news', 'news'), ('faq', 'faq'), ('longread', 'longread')]
+    ITER_KEYS = ['id', 'slug', 'format', 'title']
+
     slug = models.CharField(max_length=120, db_index=True)
-    format = models.CharField(max_length=10, db_index=True)
+    format = models.CharField(max_length=10, choices=FORMAT_CHOICES, db_index=True)
     title = models.CharField(max_length=120)
     create_date = models.DateTimeField(auto_now_add=True, editable=False)
     text = models.TextField(blank=True)
 
     def __str__(self):
-        return f"{self.title}"
-
-    def __unicode__(self):
         return f"{self.format}/{self.slug}"
 
+    def __iter__(self):
+        for k in self.ITER_KEYS:
+            yield k, object.__getattribute__(self, k)
--- articles/views.py
+++ articles/views.py
@@ -6,24 +6,30 @@ from django.http import HttpResponse
 from .models import Article
 
 
-name_re = re.compile(r"^(?P<format>(news|faq|longread))/(?P<slug>[^/\s]+)$")
+name_re = re.compile(r"^(?P<format>(" + '|'.join([f[1] for f in Article.FORMAT_CHOICES]) + "))/(?P<slug>[^/\s]+)$")
 
 def articles_list(request):
     if request.method != 'GET':
         return HttpResponse(f"Method {request.method} not supported", status=405)
 
-    data = []
+    data, names, slugs = {}, [], set()
     # TODO в случае отсуствия names неплохо бы отдавать все статьи, но при наличии pagination
     for name in request.GET.get('names', '').split(','):
         if not name:
             continue
-        try:
-            name_dict = name_re.match(name).groupdict()
-            article = Article.objects.get(**name_dict)
-        except AttributeError:
+        match = name_re.match(name)
+        if not match:
             return HttpResponse(f"Name '{name}' not supported", status=400)
-        except Article.DoesNotExist:
-            return HttpResponse(f"article '{name}' not exist", status=404)
-        data.append({'id': article.id, 'slug': article.slug, 'format': article.format, 'title': article.title})
+        names.append(name)
+        slugs.add(match.groupdict()['slug'])
+    if slugs:
+        for article in Article.objects.filter(slug__in=list(slugs)):
+            data[str(article)] = article
+        not_found_names = set(names) - set(data.keys())
+        if not_found_names:
+            return HttpResponse(f"Articles {list(not_found_names)} not found", status=404)
 
-    return HttpResponse(json.dumps({'status': 'ok', 'data': data}, indent=4), content_type="application/json")
+    return HttpResponse(
+        json.dumps({'status': 'ok', 'data': [dict(data[name]) for name in names]}, indent=4),
+        content_type="application/json"
+    )
